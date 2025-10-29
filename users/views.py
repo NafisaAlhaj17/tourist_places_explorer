bd48e214faf54f3a47cf67bd3a61fb22b734a73d
Nafisa Alhaj <alhajnafisa17@gmail.com>
2025-10-29 14:27:39 +0530

    Add login and logout views with messages

--- users/views.py
+++ users/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from .forms import SignUpForm
-from django.contrib.auth import login
 
+# Sign Up View
 def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, "Your account has been created successfully!")
             return redirect('places:place_list')
     else:
         form = SignUpForm()
     return render(request, 'users/signup.html', {'form': form})
+
+#  Login View
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "You are now logged in!")
+            return redirect('places:place_list')
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+#  Logout View
+def custom_logout(request):
+    logout(request)
+    messages.info(request, "You have been logged out successfully.")
+    return redirect('/')
